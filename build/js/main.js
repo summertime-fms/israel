36ce9a0c63716b75dd6f5990550a8b38648307a0
summertime-fms <danyushin0107@gmail.com>
2020-07-02 12:51:55 +0300

    Добавляет оформление и валидацию формы модального окна, адаптирует под все разрешения

--- build/js/main.js
+++ build/js/main.js
@@ -1,47 +1,76 @@
-// let form = document.querySelector('.form');
-// let input = form.querySelectorAll('.form__input')
-
-// let inputWrappers = form.querySelectorAll('.form__input-wrapper');
-
-// for (let i = 0; i < input.length; i++) {
-//   input[i].addEventListener('focus', function() {
-//     inputWrappers[i].style.display = "block";
-//   })
-// }
+// POPUP
 
 let orderCallLink = document.querySelector('.page-header__order');
 let modalCall = document.querySelector('.modal-call');
 let modalCallClose = modalCall.querySelector('.modal-call__close');
 let overlay = document.querySelector('.overlay');
 
-orderCallLink.addEventListener('click', function(evt) {
-  evt.preventDefault();
+let onEscPress = function(evt) {
+  // evt.preventDefault();
+  if (evt.key === 'Escape') {
+    closePopup();
+}}
+
+let openPopup = function() {
   modalCall.classList.add('modal-call--open');
   overlay.classList.add('overlay--show');
-  let clientRect = modalCall.getBoundingClientRect();
-  let modalCallWidth = clientRect.width;
-  let modalCallHeight = clientRect.height;
-  let modalTop = 'calc(50% - ' + modalCallHeight / 2 + 'px)';
-  let modalLeft = 'calc(50% - ' + modalCallWidth / 2 + 'px)';
-  modalCall.style.left = modalLeft;
-  console.log(modalLeft);
 
-  modalCall.style.top = modalTop;
+  document.addEventListener('keydown', onEscPress);
+  overlay.addEventListener('click', closePopup);
+}
+
+let closePopup = function() {
+  modalCall.classList.remove('modal-call--open');
+  overlay.classList.remove('overlay--show');
+  document.removeEventListener('keydown', onEscPress);
+  overlay.removeEventListener('click', closePopup);
+}
 
 
+let centerElement = function(element) {
+  let clientRect = element.getBoundingClientRect();
+  let elementWidth = clientRect.width;
+  let elementHeight = clientRect.height;
+  let elementTop = 'calc(50% - ' + elementHeight / 2 + 'px)';
+  let elementLeft = 'calc(50% - ' + elementWidth / 2 + 'px)';
+  element.style.top = elementTop;
+  element.style.left = elementLeft;
+}
+
+orderCallLink.addEventListener('click', function() {
+  openPopup();
+  centerElement(modalCall);
 })
 
 modalCallClose.addEventListener('click', function(){
-  modalCall.classList.remove('modal-call--open');
-  overlay.classList.remove('overlay--show');
+  closePopup();
 })
 
-window.addEventListener('keydown', function(){
 
-  if (key.name = 'Esc') {
-  if (modalCall.classList.contains('modal-call--open')) {
-    modalCall.classList.remove('modal-call--open');
-    overlay.classList.remove('overlay--show');
+
+
+// VALIDATION
+
+
+
+let form = document.querySelector('.form');
+let userNameInput = form.querySelector('.form__input--name');
+
+userNameInput.addEventListener('invalid', function(evt) {
+  if (userNameInput.validity.tooShort) {
+userNameInput.setCustomValidity('Имя минимум из 2 символов');
   }
-}
+  else if (userNameInput.validity.tooLong) {
+    userNameInput.setCustomValidity('Имя не может превышать 25 символов')
+  } else if (userNameInput.validity.valueMissing) {
+    userNameInput.setCustomValidity('Обязательное поле')
+  } else {
+    userNameInput.setCustomValidity('');
+  }
 })
+
+//PHONE-NUMBER INPUT MASK
+
+$(".form__input--phone").mask("8 (999) 999 99 99");
+
+
