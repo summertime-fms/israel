74ebcea882123404606d8e0367c2fee15bcbf535
summertime-fms <danyushin0107@gmail.com>
2020-07-13 12:58:23 +0300

    убирает открытый вопрос в блоке faq

--- build/js/main.js
+++ build/js/main.js
@@ -1,5 +1,3 @@
-
-
 //POLYFILLS
 (function() {
 
@@ -166,36 +164,46 @@ modalRecallButton.addEventListener('click', function() {
 
 // TABS
 
+let tabsList = document.querySelector('.programs__tabs');
+let tabs = tabsList.querySelectorAll('.programs__tab');
+tabsList.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  let items = document.querySelectorAll('.programs__item');
 
-var tabs = document.querySelectorAll('.programs__tab');
-var texts = document.querySelectorAll('.programs__item');
-
-let showTab = function(item) {
-  for (let i = 0; i < tabs.length; i++)  {
-    tabs[i].classList.remove('programs__tab--active');
-  }
-  item.classList.add('programs__tab--active');
-}
-let showText = function(ind) {
-  for (let i = 0; i < texts.length; i++) {
-      texts[i].classList.remove('programs__item--active');
-    if (i === ind) {
-      texts[i].classList.add('programs__item--active')
+  let target = evt.target.closest('li');
+  if (!target) return;
+  for (let i = 0; i < tabs.length; i++) {
+    if (tabs[i].classList.contains('programs__tab--active')) {
+      tabs[i].classList.remove('programs__tab--active')
     }
   }
-}
+  for (let i = 0; i < items.length; i++) {
+    if (items[i].classList.contains('programs__item--active')) {
+      items[i].classList.remove('programs__item--active')
+    }
 
-let operateTab = function(tab, index) {
-  tab.addEventListener('click', function(evt) {
-    showTab(tab);
-    showText(index);
-  })
+  }
+  target.classList.add('programs__tab--active');
 
-}
+  if(target.classList.contains('programs__tab--common')) {
+    items[0].classList.add('programs__item--active');
+  }
+  if(target.classList.contains('programs__tab--academic')) {
+    items[1].classList.add('programs__item--active');
+  }
+  if(target.classList.contains('programs__tab--internship')) {
+    items[2].classList.add('programs__item--active');
+  }
+  if(target.classList.contains('programs__tab--volunteur')) {
+    items[3].classList.add('programs__item--active');
+  }
+  if(target.classList.contains('programs__tab--religion')) {
+    items[4].classList.add('programs__item--active');
+  }
+})
 
-for (let i = 0; i < tabs.length; i++) {
-  operateTab(tabs[i], i);
-}
+
+
 
 
 //CALL BUTTON POPUP
